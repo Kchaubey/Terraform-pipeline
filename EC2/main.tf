54d8fd0d2a837e98460c688a1b978aee4f0dd1bd
Kajal Chaubey <88192782+Kchaubey@users.noreply.github.com>
2024-10-24 23:54:04 +0530

    Create main.tf

---
+++ EC2/main.tf
@@ -0,0 +1,42 @@
+resource "aws_instance" "jenkins" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.public.id
+  key_name      = var.key_name
+  security_groups = [aws_security_group.jenkins_sg.id]
+
+  tags = {
+    Name = "Jenkins Server"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y java-1.8.0-openjdk-devel
+              wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
+              rpm --import https://pkg.jenkins.io/redhat/jenkins.io.key
+              yum install -y jenkins
+              systemctl start jenkins
+              systemctl enable jenkins
+              EOF
+}
+
+resource "aws_instance" "docker" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.public.id
+  key_name      = var.key_name
+  security_groups = [aws_security_group.docker_sg.id]
+
+  tags = {
+    Name = "Docker Host"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              yum update -y
+              yum install -y docker
+              systemctl start docker
+              systemctl enable docker
+              EOF
+}
