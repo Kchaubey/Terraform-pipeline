4e60a7cfad0a45a9b61aaa5fd156e640c7073ff6
Kajal Chaubey <88192782+Kchaubey@users.noreply.github.com>
2024-10-25 00:07:42 +0530

    Create provider.tf for global

---
+++ provider.tf
@@ -0,0 +1,24 @@
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.11.0"
+    }
+  }
+}
+provider "aws" {
+  region = "us-west-2"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  default = "ami-0ddc798b3f1a5117e" # Replace with a valid AMI
+}
+
+variable "key_name" {
+  default = "Jenkins" # Replace with your SSH key name
+}
